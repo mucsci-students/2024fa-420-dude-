70748b2c2cbf1d45c1779243002697af6c615d59
HunterInPlainText <huntergweav@gmail.com>
2024-09-11 18:51:40 -0400

    Fixed the programm crashing when not providing input.

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 # This is the file that will be used for programming the interface.
 
 # Command options printed if user inputs "help"
-options = '''Options:
+options = '''Commmands:
     lscls - List all classes and their attributes
     clsinfo [Class] - Provides information on the [Class] 
     lsrel - List all relationships between classes
@@ -11,7 +11,7 @@ options = '''Options:
 # Warns the user that they have to manny arguments
 def uneaded_argument_warning(command: list):
     if len(command) > 1: 
-        print(command[0] + "does not have options")
+        print(command[0] + " does not have options")
 
 print("Enter a command, \nUse \"help\" for information")
 
@@ -19,14 +19,17 @@ print("Enter a command, \nUse \"help\" for information")
 #   DUML stands for Dude UML
 user_input = input("DUML:  ")
 command = user_input.split()
-while command[0] != "exit": # Continues to prompt the user for input until provided "exit"
+if len(command) == 0:
+    command = [" "]
+while command[0] != "exit":
     match command[0]:
         case "lscls":
             uneaded_argument_warning(command)   
             print("lscls is not yet implemented")
         case "clsinfo":
-            if len(command <= 1):
+            if len(command) <= 1:
                 print("Please provide a valid class to display")
+                # Need to implement a print statement if the class provided is not a valid class
                 break
             print("clsinfo is not yet implemented")
         case "lsrel":
@@ -35,10 +38,14 @@ while command[0] != "exit": # Continues to prompt the user for input until provi
         case "help":
             uneaded_argument_warning(command)
             print(options)
+        case " ":
+            print("Please provide a command")
         case _: # Default case if others did not match
             print(command[0] + " is not a command\n\tUse \"help\" for a list of valid commands")
-    user_input = input("DUML:  ") 
+    user_input = input("DUML:  ")
     command = user_input.split()
+    if len(command) == 0:
+        command = [" "]
 
 # Exits the program
 exit(0)
