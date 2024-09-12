66d9c764983baa3796cc549b51211f6e0aa40e38
derrickboyer3 <derrickboyer3455@gmail.com>
2024-09-12 12:49:53 -0400

    Added base functions for creating, adding projects and creating, adding, renaming classes

--- MongoFunctions.py
+++ MongoFunctions.py
@@ -20,14 +20,49 @@ def connect():
         else:
             return collection
         
+
+
+
+
+# Project Functions
+# Function to create a new project object
+def create_project(collection, project_data):
+    collection.insert_one(project_data)
+
+# Function to get a project object
+def get_project(collection, project_name):
+    data = collection.find_one({ "object type": "project", "name": project_name })
+    return data
+
+
+
+
+# Class Functions
 # Function to create a new class object
 def create_class(collection, class_data):
     collection.insert_one(class_data)
 
+# Function to get a class object
+def get_class(collection, project_name, class_name):
+    data = collection.find_one({ "object type": "class", "project": project_name, "name": class_name })
+    return data
+
+#Function to rename a class object
+def rename_class(collection, project_name, class_name, new_name):
+    collection.update_one({ "object type": "class", "project": project_name, "name": class_name }, { "$set": { "name": new_name } })
+
+
+
+# Relationship Functions
 # Function to create a new relationship object
 def create_relationship(collection, relationship_data):
     collection.insert_one(relationship_data)
 
+
+
+
+
+# Attribute Functions
 # Function to create a new attribute object
 def create_attribute(collection, attribute_data):
     collection.insert_one(attribute_data)
--- MongoFunctionsTestFile.py
+++ MongoFunctionsTestFile.py
@@ -1,5 +1,9 @@
 import MongoFunctions
 
+
+
+
+
 # Test connecting to the database
 collection = MongoFunctions.connect()
 if collection is None:
@@ -8,19 +12,44 @@ if collection is None:
 else:
     print("Welcome to the UML Editor!")
 
-exit = False
-while not exit:
-      print("What would you like to do?\nCreate a new class => C\nCreate a new relationship => R\nCreate a new attribute\nQuit => Q")
-      choice = input()
-      match choice:
-            case "C":
-                MongoFunctions.create_class(collection, class_data)
-            case "R":
-                MongoFunctions.create_relationship(collection, relationship_data)
-            case "A":
-                MongoFunctions.create_attribute(collection, attribute_data)
-            case "Q":
-                exit = True
+
+
+
+
+# Project functions tests
+# Test adding a Project to the database
+project_object = {
+    "object type": "project",
+    "name": "Test Project",
+}
+MongoFunctions.create_project(collection, project_object)
+
+# Test getting a Project from the database
+project_object = MongoFunctions.get_project(collection, "Test Project")
+print(project_object)
+
+
+
+
+
+# Class functions tests
+# Test adding a Class to the database
+class_object = {
+    "object type": "class",
+    "project": "Test Project",
+    "name": "TestClass",
+    "attributes": [{ "object type": "attribute", "name": "testAttribute1", "type": "int", "value": 0 }],
+}
+MongoFunctions.create_class(collection, class_object)
+
+# Test getting a class from the database
+class_object = MongoFunctions.get_class(collection, "Test Project", "TestClass")
+print(class_object)
+
+# Test renaming a class in the database
+MongoFunctions.rename_class(collection, "Test Project", "TestClass", "NewTestClass")
+class_object2 = MongoFunctions.get_class(collection, "Test Project", "NewTestClass")
+print(class_object2)
  
 
 
