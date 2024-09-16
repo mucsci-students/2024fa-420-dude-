93c24e21b2e9bdbc4afc4eb4ad2ff762a32c8d22
derrickboyer3 <derrickboyer3455@gmail.com>
2024-09-16 09:09:47 -0400

    Mongo Database Functions seem to be finished, along with the tests, they just need to be incorporated into the main code

--- MongoFunctions.py
+++ MongoFunctions.py
@@ -34,6 +34,11 @@ def get_project(collection, project_name):
     data = collection.find_one({ "object type": "project", "name": project_name })
     return data
 
+# Function to delete a project object
+def delete_project(collection, project_name):
+    collection.delete_one({ "object type": "project", "name": project_name })
+
+
 
 
 
@@ -51,6 +56,12 @@ def get_class(collection, project_name, class_name):
 def rename_class(collection, project_name, class_name, new_name):
     collection.update_one({ "object type": "class", "project": project_name, "name": class_name }, { "$set": { "name": new_name } })
 
+# Function to delete a class object
+def delete_class(collection, project_name, class_name):
+    collection.delete_one({ "object type": "class", "project": project_name, "name": class_name })
+
+
+
 
 
 # Relationship Functions
@@ -58,14 +69,34 @@ def rename_class(collection, project_name, class_name, new_name):
 def create_relationship(collection, relationship_data):
     collection.insert_one(relationship_data)
 
+# Function to get a relationship object
+def get_relationship(collection, project_name, relationship_type, class1_name, class2_name):
+    data = collection.find_one({ "object type": "relationship", "project": project_name, "relationship type": relationship_type, "class1": class1_name, "class2": class2_name })
+    return data
+
+# Function to delete a relationship object
+def delete_relationship(collection, project_name, relationship_type, class1_name, class2_name):
+    collection.delete_one({ "object type": "relationship", "project": project_name, "relationship type": relationship_type, "class1": class1_name, "class2": class2_name })
+
+
 
 
 
 
 # Attribute Functions
 # Function to create a new attribute object
-def create_attribute(collection, attribute_data):
-    collection.insert_one(attribute_data)
+def create_attribute(collection, project_name, class_name, attribute_data):
+    collection.find_one_and_update({ "object type": "class", "project": project_name, "name": class_name }, { "$push": { "attributes": attribute_data } })
+
+# Function to rename an attribute object
+def rename_attribute(collection, project_name, class_name, attribute_name, new_name):
+    collection.find_one_and_update({ "object type": "class", "project": project_name, "name": class_name, "attributes.name": attribute_name }, { "$set": { "attributes.$.name": new_name } })
+
+# Function to delete an attribute object
+def delete_attribute(collection, project_name, class_name, attribute_name):
+    collection.find_one_and_update({ "object type": "class", "project": project_name, "name": class_name }, { "$pull": { "attributes": { "name": attribute_name } } })
+
+
 
     
 
--- MongoFunctionsTestFile.py
+++ MongoFunctionsTestFile.py
@@ -50,6 +50,54 @@ print(class_object)
 MongoFunctions.rename_class(collection, "Test Project", "TestClass", "NewTestClass")
 class_object2 = MongoFunctions.get_class(collection, "Test Project", "NewTestClass")
 print(class_object2)
+
+
+
+
+
+# Relationship functions tests
+# Test adding a Relationship to the database
+relationship_object = {
+    "object type": "relationship",
+    "project": "Test Project",
+    "relationship type": "association",
+    "class1": "NewTestClass",
+    "class2": "TestClass",
+}
+MongoFunctions.create_relationship(collection, relationship_object)
+data = MongoFunctions.get_relationship(collection, "Test Project", "association", "NewTestClass", "TestClass")
+print(data)
+
+# Test deleting a Relationship from the database
+MongoFunctions.delete_relationship(collection, "Test Project", "association", "NewTestClass", "TestClass")
+
+
+
+
+
+# Attribute functions tests
+# Test adding an attribute to a class in the database
+attribute_object = {
+    "object type": "attribute",
+    "name": "testAttribute2",
+    "type": "string",
+    "value": "test value",
+}
+MongoFunctions.create_attribute(collection, "Test Project", "NewTestClass", attribute_object)
+data = MongoFunctions.get_class(collection, "Test Project", "NewTestClass")
+print(data)
+
+# Test renaming an attribute in the database
+MongoFunctions.rename_attribute(collection, "Test Project", "NewTestClass", "testAttribute2", "newTestAttribute2")
+data = MongoFunctions.get_class(collection, "Test Project", "NewTestClass")
+print(data)
+
+# Test deleting an attribute from a class in the database
+MongoFunctions.delete_attribute(collection, "Test Project", "NewTestClass", "testAttribute1")
+data = MongoFunctions.get_class(collection, "Test Project", "NewTestClass")
+print(data)
+
+
  
 
 
